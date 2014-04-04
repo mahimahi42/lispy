475b6d36857562824a919547b78f224ec5b0e935
Bryce Davis <bad1128@gmail.com>
2014-04-04 14:45:10 -0400

    Add preprocessor check for Windows

--- prompt.c
+++ prompt.c
@@ -1,8 +1,30 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/* Compile these functions if we're on Windows */
+#ifdef _WIN32
+
+#include <string.h>
+
+static char buffer[2048];
+
+char* readline(char* prompt) {
+	fputs(prompt, stdout);
+	fgets(buffer, 2048, stdin);
+	char* cpy = malloc(strlen(buffer) + 1);
+	strcpy(cpy, buffer);
+	cpy[strlen(cpy) - 1] = "\0";
+	return cpy;
+}
+
+void add_history(char* unused) {}
+
+#else
+
 #include <editline/readline.h>
 #include <editline/history.h>
+
+#endif
 
 int main(int argc, char** argv) {
 	/* Print Version and Exit Information */
