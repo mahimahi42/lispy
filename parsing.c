7ba57a3c8ad3ecdfda453d67676b7c4d764aaf60
Bryce Davis <bad1128@gmail.com>
2014-04-04 15:08:32 -0400

    Implement Polish Notation parser

--- parsing.c
+++ parsing.c
@@ -29,6 +29,23 @@ void add_history(char* unused) {}
 #endif
 
 int main(int argc, char** argv) {
+	/* Create parsers */
+	mpc_parser_t* Number   = mpc_new("number");
+	mpc_parser_t* Operator = mpc_new("operator");
+	mpc_parser_t* Expr     = mpc_new("expr");
+	mpc_parser_t* Lispy    = mpc_new("lispy");
+
+	/* Define parsers */
+	mpca_lang(MPC_LANG_DEFAULT,
+		"                                                     \
+		number   : /-?[0-9]+/ ;                             \
+		operator : '+' | '-' | '*' | '/' ;                  \
+		expr     : <number> | '(' <operator> <expr>+ ')' ;  \
+		lispy    : /^/ <operator> <expr>+ /$/ ;             \
+		",
+		Number, Operator, Expr, Lispy);
+
+
 	/* Print Version and Exit Information */
 	puts("Lispy Version 0.0.0.0.1");
 	puts("Press Ctrl+c to Exit\n");
@@ -41,12 +58,24 @@ int main(int argc, char** argv) {
 	    /* Add input to history */
 	    add_history(input);
 	    
-	    /* Echo input back to user */    
-	    printf("No you're a %s\n", input);
+	    /* Attempt to parse input */
+	    mpc_result_t r;
+	    if (mpc_parse("<stdin>", input, Lispy, &r)) {
+	    	/* Print AST on success */
+	    	mpc_ast_print(r.output);
+	    	mpc_ast_delete(r.output);
+	    } else {
+	    	/* Otherwise, print the error */
+	    	mpc_err_print(r.error);
+	    	mpc_err_delete(r.error);
+	    }
 
 	    /* Free retrived input */
 	    free(input);
 	}
+
+	/* Undefine and delete parsers */
+	mpc_cleanup(4, Number, Operator, Expr, Lispy);
 
 	return 0;
 }
